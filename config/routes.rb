00c33f50fdccfb428b1da8fd0fd59edd45322067
SuryaSankar <suryashankar.m@gmail.com>
2014-03-15 17:14:24 +0530

    first working version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Moocpp::Application.routes.draw do
+  root 'pages#home'
+  resources :courses
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
