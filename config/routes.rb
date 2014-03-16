766bed381abb9b40c9416fe92062b66eac2a8952
SuryaSankar <suryashankar.m@gmail.com>
2014-03-16 10:56:15 +0530

    added files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Moocpp::Application.routes.draw do
   root 'pages#home'
   resources :courses
+  get '/search' => 'pages#search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
